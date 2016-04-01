fa4c2158c28ec8f3d9a6a1d7fd4b2b0bdeddae6c
Scott Christensen <mscottchristensen@gmail.com>
2016-03-31 21:55:22 -0600

    added just about all of the wpf functionality that I want for this iteration

--- WPFSerialApplication/StewartPlatformSerialController/MainWindow.xaml.cs
+++ WPFSerialApplication/StewartPlatformSerialController/MainWindow.xaml.cs
@@ -35,7 +35,10 @@ namespace StewartPlatformSerialController
 
         private void SendButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (serialConnection.IsOpen)
+                SendMessage();
+            else
+                MessageBox.Show("You need to connect first");
         }
 
         private void QuitButton_Click(object sender, RoutedEventArgs e)
@@ -79,6 +82,119 @@ namespace StewartPlatformSerialController
         {
             if (ComboBoxSerialPorts.SelectedIndex != -1)
                 OpenSerialConnection();
+            else
+                MessageBox.Show("There is no selected port to connect to");
+        }
+
+        private void ComboBoxSerialPorts_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            availableComPorts = new List<string>(SerialPort.GetPortNames().ToList());
+            ComboBoxSerialPorts.ItemsSource = availableComPorts;
+        }
+
+        private void TextBox_RollAngle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(this.IsLoaded)
+                if(CheckBox_SendContinuously.IsChecked.HasValue)
+                    if((bool)CheckBox_SendContinuously.IsChecked)
+                        SendMessage();
+        }
+
+        private void TextBox_ZTranslation_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.IsLoaded)
+                if (CheckBox_SendContinuously.IsChecked.HasValue)
+                    if ((bool)CheckBox_SendContinuously.IsChecked)
+                        SendMessage();
+        }
+
+        private void TextBox_YTranslation_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.IsLoaded)
+                if (CheckBox_SendContinuously.IsChecked.HasValue)
+                    if ((bool)CheckBox_SendContinuously.IsChecked)
+                        SendMessage();
+        }
+
+        private void TextBox_XTranslation_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.IsLoaded)
+                if (CheckBox_SendContinuously.IsChecked.HasValue)
+                    if ((bool)CheckBox_SendContinuously.IsChecked)
+                        SendMessage();
+        }
+
+        private void TextBox_YawAngle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.IsLoaded)
+                if (CheckBox_SendContinuously.IsChecked.HasValue)
+                    if ((bool)CheckBox_SendContinuously.IsChecked)
+                        SendMessage();
+        }
+
+        private void TextBox_PitchAngle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.IsLoaded)
+                if (CheckBox_SendContinuously.IsChecked.HasValue)
+                    if ((bool)CheckBox_SendContinuously.IsChecked)
+                        SendMessage();
+        }
+
+        private void Slider_RollAngle_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBox_RollAngle.Text = Math.Round(Convert.ToSingle(e.NewValue.ToString()), 3).ToString();
+        }
+
+        private void Slider_ZTranslation_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBox_ZTranslation.Text = Math.Round(Convert.ToSingle(e.NewValue.ToString()), 3).ToString();
+        }
+
+        private void Slider_YTranslation_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBox_YTranslation.Text = Math.Round(Convert.ToSingle(e.NewValue.ToString()), 3).ToString();
+        }
+
+        private void Slider_XTranslation_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBox_XTranslation.Text = Math.Round(Convert.ToSingle(e.NewValue.ToString()),3).ToString();
+        }
+
+        private void Slider_YawAngle_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBox_YawAngle.Text = Math.Round(Convert.ToSingle(e.NewValue.ToString()), 3).ToString();
+        }
+
+        private void Slider_PitchAngle_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBox_PitchAngle.Text = Math.Round(Convert.ToSingle(e.NewValue.ToString()), 3).ToString();
+        }
+
+        private void CheckBox_SendContinuously_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!serialConnection.IsOpen)
+            {
+                MessageBox.Show("Can't send messages until you connect to a serial port");
+                (e.Source as CheckBox).IsChecked = false;
+            }
+            else
+                SendMessage();
+        }   
+
+        private void SendMessage()
+        {
+            if (serialConnection.IsOpen)
+            {
+                //maybe something like this...?
+                string message = null;
+                message += TextBox_PitchAngle + "\n";
+                message += TextBox_RollAngle + "\n";
+                message += TextBox_YawAngle + "\n";
+                message += TextBox_XTranslation + "\n";
+                message += TextBox_YTranslation + "\n";
+                message += TextBox_ZTranslation;
+                serialConnection.Write(message);
+            }
         }
     }
 }
